2a96f62224daa4e588a1e3766811f6fb4b47e49b
Paolo Montalto <montypablo@gmail.com>
2017-09-14 11:53:32 +0200

    Added PHP classes

---
+++ Logger.class.php
@@ -0,0 +1,106 @@
+<?php
+
+#
+# This file is part of Logger for PHP.
+
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+/**
+ * Class Logger v. 1.0
+ */
+class Logger {
+	private $path;
+	private $name;
+	private $caller;
+	private $config;
+
+    public function __construct($c, LoggerConfig $config = null) {
+        $this->config = $config;
+        if ( $this->config == null )
+            $this->$config = LoggerConfig::getDefaultConfig();
+
+        $this->caller = $c;
+        $this->path = $config->logFilePath;
+        $this->name = $config->logNamePrefix . "_" . date("y-m-d") . ".log";
+    }
+	
+	public function info($text){
+		try {
+			if (LogLevel::INFO >= $this->logLevel){
+				$this->append("I/" . get_class($this->caller) .  " [ " . date("d/M/y H:i:s") . " ]: " . utf8_decode($text));
+			}
+		} catch (Exception $e) {
+			print $e->getMessage();
+		}
+	}
+	
+	public function debug($text){
+		try {
+			if (LogLevel::DEBUG >= $this->logLevel){
+				$this->append("D/" . get_class($this->caller) .  " [ " . date("d/M/y H:i:s") . " ]: " . utf8_decode($text));
+			}
+		} catch (Exception $e) {
+			print $e->getMessage();
+		}
+	}
+	
+	public function error($text){
+		try {
+			if (LogLevel::ERROR >= $this->logLevel){
+				$this->append("E/" . get_class($this->caller) .  " [ " . date("d/M/y H:i:s") . " ]: " . utf8_decode($text));
+			}
+		} catch (Exception $e) {
+			print $e->getMessage();
+		}
+	}
+	
+	public function fatal($text){
+		try {
+			if (LogLevel::FATAL >= $this->logLevel){
+				$this->append("F/" . get_class($this->caller) .  " [ " . date("d/M/y H:i:s") . " ]: " . utf8_decode($text));
+			}
+		} catch (Exception $e) {
+			print $e->getMessage();
+		}
+	}
+	
+	public function warn($text){
+		try {
+			if (LogLevel::WARN >= $this->logLevel){
+				$this->append("W/" . get_class($this->caller) .  " [ " . date("d/M/y H:i:s") . " ]: " . utf8_decode($text));
+			}
+		} catch (Exception $e) {
+			print $e->getMessage();
+		}
+	}
+	
+	public function trace($text){
+		try {
+			if (LogLevel::TRACE >= $this->logLevel){
+				$this->append("T/" . get_class($this->caller) .  " [ " . date("d/M/y H:i:s") . " ]: " . utf8_decode($text));
+			}
+		} catch (Exception $e) {
+			print $e->getMessage();
+		}
+	}
+	
+	private function append($text) {
+		$this->logFile = fopen($this->path . $this->name, 'a+');
+		fwrite($this->logFile, $text);
+		fwrite($this->logFile, "\n");
+		fclose($this->logFile);
+	}
+}
+	
+?>
